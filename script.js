b9a8506d8dac877dc4ee93d5fdeca9db98d5ddc0
Pedro Henrique de Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-08 17:49:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,10 @@ function paginate(SelectedPage , totalPages) {//algoritmo de paginação no fron
             if (oldPage && currentPage - oldPage > 2) {//adicionando os pontinhos
                 page.push('...')
             }
-            if (oldPage && currentPage - oldPage == 2) {//caso o oldpage não seja maior que 2
+            if (oldPage && currentPage - oldPage == 2) {//caso o oldpage não seja maior que 2 assim:
+                                                        // [ 1 , ... , 14,15, 16 ,17,18 , 20]
+                                                        //iremos adicionar mais um assim
+                                                        //[ 1 , ... , 14,15, 16 ,17,18 , 19,20]
                 page.push(oldPage + 1)
             }
 
@@ -27,25 +30,34 @@ function paginate(SelectedPage , totalPages) {//algoritmo de paginação no fron
 
 //enviamos através do back-end as infos
 //e vamos influenciar o front atraves do javascript
-let pagination = document.querySelector(".pagination")
 
-const page = +pagination.dataset.page//pegando os dados no html atraves da variavel que pega a div 
-const total = +pagination.dataset.total
+function createPagination(pagination){
+    const page = +pagination.dataset.page//pegando os dados no html atraves da data-(no javascript é dataset) que pega a div 
+    const total = +pagination.dataset.total
 
-const pages = paginate(page, total)//chamnado a função
+    const pages = paginate(page, total)//chamnado a função
 
-let elemetns = ""//elemento vazio por inicio
+    let elemetns = ""//elemento vazio por inicio
 
-for(let page of pages){//repetição
-    if(String(page).includes('...')){//o includes só funciona com strings portanto
-                                    //tranformaremos o que foi passado as info em string
-                                    //e usaremos o includes para procurar os pontinhos
-        elemetns += `<span>${page}</span>`
+    for(let page of pages){//repetição
+        if(String(page).includes('...')){//o includes só funciona com strings portanto
+                                        //tranformaremos o que foi passado as info em string
+                                        //e usaremos o includes para procurar os pontinhos
+                                        //e mudamos de um links para algo que não seja clicavel
+            elemetns += `<span>${page}</span>`
 
-    }else{
-        elemetns += `<a href="?page=${page}">${page}</a>`
+        }else{
+            elemetns += `<a href="?page=${page}">${page}</a>`
 
-    }
+        }
+     }
+pagination.innerHTML = elemetns//adicionando ha div(com class pagination) a let elements quem tem as quantidades de pages
+    
 }
 
-pagination.innerHTML = elemetns//adicionando ha div(com class pagination) a let elements quem tem as quantidades de pages
+let pagination = document.querySelector(".pagination")//pagination é uma div
+
+
+if(pagination){
+    createPagination(pagination)
+}
