15b082a91d34d979ca614c43cfd6899fd2a6bbae
Pedro Henrique de Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-08 17:59:55 -0300

    Update controller.js

--- controller.js
+++ controller.js
@@ -15,20 +15,32 @@ module.exports = {
     //e vai fazer um a multiplicação
     //e assim offset vai receber
     
-    let offset = limit * (page - 1)
+    let offset = limit * (page - 1) // vai sempre aumentando de 2 em 2
    
     //LIMIT = o número de id que pode aparecer
     //OFFSET = começa a contar de um determinado número
     
     //agruparemos tudo em uma variavel para ficar mais organizado
-    const params = {
-      filter,
-      page,
-      limit,
-      offset,
-      callback(instructors){
-        return res.render('instructors/index' , { instructors , filter})
-      }
+   const params  = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(instructors){
+                const pagination = {
+                    page, //page já temos tiramos do req.query
+                    total: Math.ceil(instructors[0].total / limit)//aqui iremos receber  o total do back-end
+                                                                  //ceil = do Math ele arredonda sempre para cima 1.1 == 2
+                                                                  //round = do math arredondamento flexivel 1.49 == 1 || 1.5 == 2
+                                                                  //floor = do math arredondamento sempre para baixo 1.5 == 1 
+                }
+                return res.render('instructors/index' , {
+                    instructors,
+                    filter,
+                    pagination
+                })
+            }
+        }
     
     Instructors.paginate(params) // fazendo a chamada da função que faz a ligação com o banco de dados
 }
